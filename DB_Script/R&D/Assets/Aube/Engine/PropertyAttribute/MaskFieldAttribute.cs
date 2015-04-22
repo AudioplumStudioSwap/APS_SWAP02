6b35ae888b531904d83f60747d961be598ef40cb
ABarangerAPS <a.baranger@audioplumstudio.com>
2015-04-22 10:55:17 +0200

    Clean

--- DB_Script/R&D/Assets/Aube/Engine/PropertyAttribute/MaskFieldAttribute.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Aube
-{
-	//!	@class	MaskField
-	//!
-	//!	@brief	Property attribute that manages an enumeration variable as a flag variable
-	public class MaskFieldAttribute : PropertyAttribute {}
-} // namespace Aube
